65782421b67571860f83c7d1e1d2be82a32cf744
jainbhavik002 <jainbhavik002@gmail.com>
2025-11-13 18:19:34 +0530

    Implement basic to-do list functionality

---
+++ to_do_list.py
@@ -0,0 +1,18 @@
+tasks = []
+
+while True:
+    print("\n1. Add task")
+    print("2. View tasks")
+    print("3. Exit")
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+    elif choice == "2":
+        for t in tasks:
+            print("-", t)
+    elif choice == "3":
+        break
+    else:
+        print("Invalid option.")
